356a6c055a2384ba07ec1fc24916aec296154319
Aviral yadav <58760825+yaviral17@users.noreply.github.com>
2024-05-12 22:31:35 +0530

    group screen changed

--- lib/Views/Home/GroupsScreen.dart
+++ lib/Views/Home/GroupsScreen.dart
@@ -114,57 +114,154 @@ class _GroupsScreenState extends State<GroupsScreen> {
           ),
           ...List.generate(
             sessionsIdList.length,
-            (index) => GestureDetector(
-              onTap: () {
-                Get.toNamed(
-                  '/group-page',
-                  arguments: sessionsIdList[index],
-                );
-              },
-              child: Container(
-                margin: const EdgeInsets.only(
-                  bottom: 12,
-                ),
-                height: TSizes.displayHeight(context) * 0.12,
-                width: double.infinity,
-                decoration: BoxDecoration(
+            (index) => Column(
+              children: [
+                //Group creation Date
+
+                Divider(
+                  height: 0,
+                  thickness: 1,
                   color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(12),
                 ),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircularCachedNetworkimage(
-                        url: sessionsIdList[index].icon,
-                        width: TSizes.displayWidth(context) * 0.2,
+                SizedBox(
+                  height: TSizes.displayHeight(context) * 0.02,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      Get.toNamed(
+                        '/group-page',
+                        arguments: sessionsIdList[index],
+                      );
+                    },
+                    child: Dismissible(
+                      key: UniqueKey(),
+                      direction: DismissDirection.startToEnd,
+                      confirmDismiss: (direction) {
+                        if (direction == DismissDirection.startToEnd) {
+                          return deleteGroup();
+                        }
+                        return Future.value(true);
+                      },
+                      //dismissible background
+                      background: Container(
+                        height: TSizes.displayHeight(context) * 0.11,
+                        margin: const EdgeInsets.only(
+                          bottom: 12,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Theme.of(context).colorScheme.error,
+                        ),
+                        padding: const EdgeInsets.only(left: 22),
+                        alignment: Alignment.centerLeft,
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                      ),
+
+                      //main container for each group
+                      child: Container(
+                        margin: const EdgeInsets.only(
+                          bottom: 12,
+                        ),
+                        height: TSizes.displayHeight(context) * 0.11,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12.0),
+                              child: Hero(
+                                tag: 'group_pfp',
+                                child: CircularCachedNetworkimage(
+                                  url: sessionsIdList[index].icon,
+                                  width: TSizes.displayWidth(context) * 0.15,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: TSizes.displayWidth(context) * 0.04,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  // sessionsIdList[index].title.capitalizeFirst ?? "",
+                                  sessionsIdList[index].title,
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                                Text(
+                                  "${sessionsIdList[index].users.length} Members",
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                Text(
+                                  "${THelperFunctions.getDateDifference(sessionsIdList[index].timestamp)}",
+                                ),
+                                // Text("₹ ${snapshot.requireData.}")
+                              ],
+                            ),
+                            Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Iconsax.menu_14),
+                            ),
+                            //add cupertino menu for share,delete and edit groups here
+                          ],
+                        ),
                       ),
                     ),
-                    SizedBox(
-                      width: TSizes.displayWidth(context) * 0.04,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          sessionsIdList[index].title.capitalizeFirst ?? "",
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        Text(
-                          THelperFunctions.getDateDifference(
-                              sessionsIdList[index].timestamp),
-                        ),
-                        // Text("₹ ${snapshot.requireData.}")
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
+          ),
+          Text(
+            "HELLO ",
+            style: Theme.of(context).textTheme.bodyLarge,
           ),
         ],
       ),
+    );
+  }
+
+  Future<bool?> deleteGroup() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Delete Group"),
+          content: const Text("Are you sure you want to delete this group?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                // FireStoreRef.leaveGroup(
+                //     sessionsIdList[index].id);
+                Navigator.pop(context, true);
+              },
+              child: const Text("Delete"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
