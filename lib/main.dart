7c5aa7f9610ffc38edf8f0c211566511ad55bea6
RJ2607 <rishijain2607@gmail.com>
2024-08-12 22:27:38 +0530

    var to permission status

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,9 @@ import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:paysa/Controllers/UserData.dart';
 import 'package:paysa/api/firebase_api.dart';
 import 'package:paysa/app.dart';
 import 'package:paysa/firebase_options.dart';
-import 'package:paysa/new/Controllers/auth_controller.dart';
-import 'package:paysa/utils/helpers/helper_functions.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -32,7 +27,10 @@ void main() async {
 }
 
 Future<void> requestStoragePermission() async {
-  var status = await Permission.storage.status;
+  PermissionStatus status = await Permission.storage.status;
+  if (status.isGranted) {
+    log('Storage permission granted');
+  }
   if (!status.isGranted) {
     status = await Permission.storage.request();
     if (status.isDenied || status.isPermanentlyDenied) {
