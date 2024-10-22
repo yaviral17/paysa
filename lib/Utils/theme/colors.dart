40dbadae6bfac053630d7a8eede504cb5b208b73
MrCodeBin <nikhilsingh12032@gmail.com>
2024-10-23 02:02:26 +0530

    added color properties

--- lib/Utils/theme/colors.dart
+++ lib/Utils/theme/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:paysa/utils/helpers/helper.dart';
+import 'package:paysa/Utils/helpers/helper.dart';
 
 class PColors {
   // App theme colors
