6abe231ed531cc5598387964b8758df80fd4f925
RJ2607 <rishijain2607@gmail.com>
2024-10-28 21:59:12 +0530

    updated profile page

--- lib/Views/Dashboard/home/profile/profile_view.dart
+++ lib/Views/Dashboard/home/profile/profile_view.dart
@@ -58,7 +58,7 @@ class ProfileView extends StatelessWidget {
                     thickness: 2,
                     height: PSize.arh(context, 3),
                   ),
-                  buildBottomProfileCard(context),
+                  buildBottomInviteCard(context),
                 ],
               )),
         ),
@@ -66,7 +66,185 @@ class ProfileView extends StatelessWidget {
     );
   }
 
-  Expanded buildBottomProfileCard(BuildContext context) {
+  Row buildProfileDetailsCard(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ZoomTapAnimation(
+              onTap: () {
+                PNavigate.back(context);
+              },
+              child: Icon(
+                Icons.arrow_back_ios_new_outlined,
+                size: PSize.arw(context, 20),
+              ),
+            ),
+            SizedBox(
+              height: PSize.arh(context, 10),
+            ),
+            Text('Aviral Yadav',
+                style: TextStyle(
+                  fontSize: PSize.arw(context, 40),
+                  fontWeight: FontWeight.bold,
+                )),
+            Text('+91 1234567890',
+                style: TextStyle(
+                  fontSize: PSize.arw(context, 20),
+                )),
+          ],
+        ),
+        Stack(children: [
+          SmoothClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            side: BorderSide(
+              color: PColors.primary(context).withOpacity(0.7),
+              width: 2,
+            ),
+            child: Image.network(
+              "https://avatars.githubusercontent.com/u/58760825?s=400&u=735ec2d81037c15adfbeea61a5a3112aef3afb85&v=4",
+              width: PSize.arw(context, 120),
+              height: PSize.arw(context, 120),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SmoothClipRRect(
+              smoothness: 0.6,
+              borderRadius: BorderRadius.circular(50),
+              side: BorderSide(
+                color: PColors.primaryDark.withOpacity(0.7),
+                width: 2,
+              ),
+              child: ZoomTapAnimation(
+                onTap: () {
+                  Get.bottomSheet(
+                    buildBottomSheet(context),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  color: PColors.primary(context),
+                  child: Icon(
+                    Icons.qr_code,
+                    color: Colors.white,
+                    size: PSize.arw(context, 25),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ],
+    );
+  }
+
+  Padding buildBottomSheet(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      child: SmoothContainer(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        smoothness: 0.6,
+        color: PHelper.isDarkMode(context)
+            ? PColors.backgroundLight
+            : PColors.backgroundDark,
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 20,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: PSize.arh(context, 20),
+            ),
+            Text(
+              'Your QR Code',
+              style: TextStyle(
+                  fontSize: PSize.arw(context, 30),
+                  color: PColors.background(context)),
+            ),
+            SizedBox(
+              height: PSize.arh(context, 20),
+            ),
+            Image.network(
+              "https://avatars.githubusercontent.com/u/58760825?s=400&u=735ec2d81037c15adfbeea61a5a3112aef3afb85&v=4",
+              width: PSize.arw(context, 200),
+              height: PSize.arw(context, 200),
+            ),
+            SizedBox(
+              height: PSize.arh(context, 20),
+            ),
+            Text(
+              'Scan this QR code to view your profile',
+              style: TextStyle(
+                  fontSize: PSize.arw(context, 20),
+                  color: PColors.background(context)),
+            ),
+            SizedBox(
+              height: PSize.arh(context, 20),
+            ),
+            GestureDetector(
+              onTap: () {
+                Get.back();
+              },
+              child: SmoothContainer(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  color: PColors.background(context),
+                  width: 1,
+                ),
+                color: PColors.primary(context),
+                child: Text(
+                  'Close',
+                  style: TextStyle(
+                    fontSize: PSize.arw(context, 25),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: PSize.arh(context, 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildProfileItems(BuildContext context) {
+    return List.generate(items.length, (index) {
+      return Column(
+        children: [
+          ZoomTapAnimation(
+            child: Text(
+              items[index],
+              style: TextStyle(
+                fontSize: PSize.arw(context, 25),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: PSize.arh(context, 20),
+          ),
+        ],
+      );
+    });
+  }
+
+  Expanded buildBottomInviteCard(BuildContext context) {
     return Expanded(
       child: Container(
         color: PHelper.isDarkMode(context)
@@ -167,191 +345,6 @@ class ProfileView extends StatelessWidget {
                 ),
               ],
             )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Row buildProfileDetailsCard(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ZoomTapAnimation(
-              onTap: () {
-                PNavigate.back(context);
-              },
-              child: Icon(
-                Icons.arrow_back_ios_new_outlined,
-                size: PSize.arw(context, 20),
-              ),
-            ),
-            SizedBox(
-              height: PSize.arh(context, 10),
-            ),
-            Text('Aviral Yadav',
-                style: TextStyle(
-                  fontSize: PSize.arw(context, 40),
-                  fontWeight: FontWeight.bold,
-                )),
-            Text('+91 1234567890',
-                style: TextStyle(
-                  fontSize: PSize.arw(context, 20),
-                )),
-          ],
-        ),
-        Stack(children: [
-          SmoothClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            side: BorderSide(
-              color: PColors.primary(context).withOpacity(0.7),
-              width: 2,
-            ),
-            child: Image.network(
-              "https://avatars.githubusercontent.com/u/58760825?s=400&u=735ec2d81037c15adfbeea61a5a3112aef3afb85&v=4",
-              width: PSize.arw(context, 120),
-              height: PSize.arw(context, 120),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: SmoothClipRRect(
-              smoothness: 0.6,
-              borderRadius: BorderRadius.circular(50),
-              side: BorderSide(
-                color: PColors.primaryDark.withOpacity(0.7),
-                width: 2,
-              ),
-              child: ZoomTapAnimation(
-                onTap: () {
-                  Get.bottomSheet(
-                    QRBottomSheet(),
-                  );
-                },
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  color: PColors.primary(context),
-                  child: Icon(
-                    Icons.qr_code,
-                    color: Colors.white,
-                    size: PSize.arw(context, 25),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ]),
-      ],
-    );
-  }
-
-  List<Widget> buildProfileItems(BuildContext context) {
-    return List.generate(items.length, (index) {
-      return Column(
-        children: [
-          ZoomTapAnimation(
-            child: Text(
-              items[index],
-              style: TextStyle(
-                fontSize: PSize.arw(context, 25),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: PSize.arh(context, 20),
-          ),
-        ],
-      );
-    });
-  }
-}
-
-class QRBottomSheet extends StatelessWidget {
-  const QRBottomSheet({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 10,
-      ),
-      child: SmoothContainer(
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        smoothness: 0.6,
-        color: PHelper.isDarkMode(context)
-            ? PColors.backgroundLight
-            : PColors.backgroundDark,
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 20,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              height: PSize.arh(context, 20),
-            ),
-            Text(
-              'Your QR Code',
-              style: TextStyle(
-                  fontSize: PSize.arw(context, 30),
-                  color: PColors.background(context)),
-            ),
-            SizedBox(
-              height: PSize.arh(context, 20),
-            ),
-            Image.network(
-              "https://avatars.githubusercontent.com/u/58760825?s=400&u=735ec2d81037c15adfbeea61a5a3112aef3afb85&v=4",
-              width: PSize.arw(context, 200),
-              height: PSize.arw(context, 200),
-            ),
-            SizedBox(
-              height: PSize.arh(context, 20),
-            ),
-            Text(
-              'Scan this QR code to view your profile',
-              style: TextStyle(
-                  fontSize: PSize.arw(context, 20),
-                  color: PColors.background(context)),
-            ),
-            SizedBox(
-              height: PSize.arh(context, 20),
-            ),
-            GestureDetector(
-              onTap: () {
-                Get.back();
-              },
-              child: SmoothContainer(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  color: PColors.background(context),
-                  width: 1,
-                ),
-                color: PColors.primary(context),
-                child: Text(
-                  'Close',
-                  style: TextStyle(
-                    fontSize: PSize.arw(context, 25),
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: PSize.arh(context, 20),
-            ),
           ],
         ),
       ),
