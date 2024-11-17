2b6e52f5e372abaf8ba23f13b4a04dc86a6c327a
RJ2607 <rishijain2607@gmail.com>
2024-11-18 02:47:28 +0530

    add card carousel feature to dashboard with navigation controls

--- lib/Controllers/dashboard_controller.dart
+++ lib/Controllers/dashboard_controller.dart
@@ -1,19 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class DashboardController extends GetxController {
   RxInt currentIndex = 0.obs;
 
+  PageController pageController = PageController(initialPage: 0);
+
   void changePage(int index) {
     currentIndex.value = index;
+    pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
   }
 
   void nextPage() {
     currentIndex.value++;
+    pageController.animateToPage(currentIndex.value,
+        duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
   }
 
   void previousPage() {
     currentIndex.value--;
-    update();
+    pageController.animateToPage(currentIndex.value,
+        duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
   }
 }
