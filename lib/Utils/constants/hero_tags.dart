36f655255eca8077833b3fd48a63eb09ebb2da2d
Aviral <yaviral17@gmail.com>
2025-01-28 01:43:12 +0530

    removed lib folder

--- lib/Utils/constants/hero_tags.dart
+++
@@ -1,9 +0,0 @@
-class PHeroTags {
-  static String appLogo = "appLogo";
-  static String authPrimaryButton = "authPrimaryButton";
-  static String authSecondaryButton = "authSecondaryButton";
-  static String authTextField = "authTextField";
-  static String authPrimaryText = "authPrimaryText";
-  static String authSecondaryText = "authSecondaryText";
-  static String homeHeader = "homeHeader";
-}
